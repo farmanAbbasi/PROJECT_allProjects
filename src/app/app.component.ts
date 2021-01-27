8fdd5fb84f3978f181077c8cf2f80689776b560d
Farman <moabbasi@teksystems.com>
2021-01-27 19:35:04 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,7 +27,14 @@ export class AppComponent implements OnInit {
       "name": "youtube",
       "link": "https://www.youtube.com/channel/UCmBdjux34PcC2rSV-z0nTBg",
       "icon":"fa fa-youtube"
+    },
+    {
+      "name": "medium",
+      "link": "https://farmanabbasi.medium.com/",
+      "icon":"fa fa-medium"
     }
+
+    
 
   ]
 
@@ -35,58 +42,58 @@ export class AppComponent implements OnInit {
 
     {
       "name": "focusnews",
-      "desc": "News App",
-      "url": "focusnews.netlify.app"
+      "desc": "News Application with language selection and search feature for any desired topic.",
+      "url": "https://focusnews.netlify.app"
     },
     {
       "name": "interflix",
-      "desc": "Movies App",
-      "url": "interflixv3.netlify.app"
+      "desc": "One stop movies watch, with popular, trending, netflix-trend, movie-news etc. features.",
+      "url": "https://interflixv3.netlify.app"
     },
     {
       "name": "tek-one",
-      "desc": "Tekathon 2020",
-      "url": "tek-one.netlify.app"
+      "desc": "Tekathon 2020, full application built within 1hr with deployment.",
+      "url": "https://tek-one.netlify.app"
     },
     {
       "name": "tvd",
-      "desc": "The Vampire Diaries",
-      "url": ".netlify.app"
+      "desc": "The Vampire Diaries all season download and stream online",
+      "url": "https://.netlify.app"
     },
     {
       "name": "interflixreviews",
-      "desc": "Review movies",
-      "url": "interflixreviews.netlify.app"
+      "desc": "Watch movies on Interflix and then review for other users.",
+      "url": "https://interflixreviews.netlify.app"
     },
     {
       "name": "rsidebar",
-      "desc": "React sidebar app",
-      "url": "rsidebar.netlify.app"
+      "desc": "Learning React with sidebar app.",
+      "url": "https://rsidebar.netlify.app"
     },
     {
       "name": "rnavbar",
-      "desc": "React navbar app",
-      "url": "rnavbar.netlify.app"
+      "desc": "Learning React navbar app.",
+      "url": "https://rnavbar.netlify.app"
     },
     {
       "name": "getgitprofile",
-      "desc": "Github profiles linked to selfcode3",
-      "url": "getgitprofile.netlify.app"
+      "desc": "Search any Github profile and then see the code from selfcode3.",
+      "url": "https://getgitprofile.netlify.app"
     },
     {
       "name": "getgitprofile2",
-      "desc": "Github profiles to github",
-      "url": "getgitprofile2.netlify.app"
+      "desc": "Search any Github profile and browse github.",
+      "url": "https://getgitprofile2.netlify.app"
     },
     {
       "name": "selfcode3",
-      "desc": "Code the github commits version 3",
-      "url": ".netlify.app"
+      "desc": "Get any public user and see repos also run Python and Java files one the spot and review what has already been pushed.",
+      "url": "https://.netlify.app"
     },
     {
       "name": "cpl2020",
-      "desc": "Code on IPL 2020 days",
-      "url": "cpl2020.netlify.app"
+      "desc": "Get Topics against IPL teams to code and learn during IPL 2020.",
+      "url": "https://cpl2020.netlify.app"
     }
 
   ]
